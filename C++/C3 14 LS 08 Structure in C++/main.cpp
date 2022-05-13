adf7d56ec63516a2ca31d73319fab507f7352a7b
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-13 16:26:00 +0530

    commit to main

--- C++/C3 14 LS 08 Structure in C++/main.cpp
+++ C++/C3 14 LS 08 Structure in C++/main.cpp
@@ -4,25 +4,27 @@ using namespace std;
 struct Book
 {
     private:
-        int bookid;
-        char title[20];
-        float price;
+	int bookid;
+	char title[20];
+	float price;
+		
     public:
-        void input()
-        {
-            cout<<"Enter BookID, Title & Price of Book: "<<endl;
-            cin>>bookid;
-            cin.ignore();
-            cin.get(title,20);
-            cin>>price;
-            cout<<endl;
-        }
-        void display()
-        {
-            cout<<endl;
-            cout<<"BookID: "<<bookid<<"\nTitle: "<<title<<"\nPrice: "<<price;
-            cout<<endl;
-        }
+	void input()
+	{
+		cout<<"Enter BookID, Title & Price of Book: "<<endl;
+		cin>>bookid;
+		cin.ignore();
+		cin.get(title,20);
+		cin>>price;
+		cout<<endl;
+    }
+	
+	void display()
+	{
+		cout<<endl;
+		cout<<"BookID: "<<bookid<<"\nTitle: "<<title<<"\nPrice: "<<price;
+		cout<<endl;
+	}
 };
 int main()
 {

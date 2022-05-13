30bf052e46f142b351b78560783fea9df68d7ee3
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-13 16:21:57 +0530

    commit to main

---
+++ C++/C3 14 LS 08 Structure in C++/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+struct Book
+{
+    private:
+        int bookid;
+        char title[20];
+        float price;
+    public:
+        void input()
+        {
+            cout<<"Enter BookID, Title & Price of Book: "<<endl;
+            cin>>bookid;
+            cin.ignore();
+            cin.get(title,20);
+            cin>>price;
+            cout<<endl;
+        }
+        void display()
+        {
+            cout<<endl;
+            cout<<"BookID: "<<bookid<<"\nTitle: "<<title<<"\nPrice: "<<price;
+            cout<<endl;
+        }
+};
+int main()
+{
+    Book b1, b2;
+    b1.input();
+    b2.input();
+    b1.display();
+    b2.display();
+    return 0;
+}

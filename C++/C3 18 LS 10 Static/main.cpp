dedfffef8d97ea9dc2f1086c7553c4bb27c0b7c8
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-15 03:59:15 +0530

    commit to main

--- C++/C3 18 LS 10 Static/main.cpp
+++ C++/C3 18 LS 10 Static/main.cpp
@@ -10,6 +10,8 @@ int main()
 {
     item i1, i2;
     i1.a=5;
-    cout<<i2.a;    //error a is not declare in scope
+    cout<<i2.a<<endl;
+    i1.k=10;    //this is technically are possible
+    cout<<i2.k; //error because k is not define
     return 0;
 }

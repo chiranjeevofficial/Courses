9967f19b0a42217784d017fbfa4ac7a8bbd0ce7f
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-15 04:05:00 +0530

    commit to main

--- C++/C3 18 LS 10 Static/main.cpp
+++ C++/C3 18 LS 10 Static/main.cpp
@@ -3,15 +3,16 @@ using namespace std;
 class item
 {
     public:
-        int a, b;
-        static int k;
+        int a, b;   //instance member variable declaration
+        static int k;   //static member variable declaration
 };
+int item::k=1;  //definition of static member variable
 int main()
 {
     item i1, i2;
     i1.a=5;
     cout<<i2.a<<endl;
-    i1.k=10;    //this is technically are possible
-    cout<<i2.k; //error because k is not define
+    i1.k=10;
+    cout<<i2.k;
     return 0;
 }

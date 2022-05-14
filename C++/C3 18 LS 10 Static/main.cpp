ea44da9e6734a79bad6427880ff7e456f8441e67
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-15 03:51:31 +0530

    commit to main

---
+++ C++/C3 18 LS 10 Static/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class item
+{
+    public:
+        int a, b;
+        static int k;
+};
+int main()
+{
+    cout<<a;    //error a is not declare in scope
+    return 0;
+}

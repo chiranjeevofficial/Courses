971590e461851be8924dd397d9c05d7825efe53e
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-15 04:10:20 +0530

    commit to main

--- C++/C3 18 LS 10 Static/main.cpp
+++ C++/C3 18 LS 10 Static/main.cpp
@@ -2,17 +2,34 @@
 using namespace std;
 class item
 {
-    public:
+    private:
         int a, b;   //instance member variable declaration
         static int k;   //static member variable declaration
+    public:
+        void set_a(int x)
+        {
+            a=x;
+        }
+        int get_a()
+        {
+            return a;
+        }
+        void set_k(int y)
+        {
+            k=y;
+        }
+        int get_k()
+        {
+            return k;
+        }
 };
 int item::k=1;  //definition of static member variable
 int main()
 {
     item i1, i2;
-    i1.a=5;
-    cout<<i2.a<<endl;
-    i1.k=10;
-    cout<<i2.k;
+    i1.set_a(5);
+    cout<<"i2.a: "<<i2.get_a()<<endl;;
+    i1.set_k(10);
+    cout<<"k: "<<i2.get_k();
     return 0;
 }

5a943db87e2fc19c201dfb463f013e2f82af8e63
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-20 11:02:41 +0530

    Update 07.cpp

--- C3/C3 10 AS 04 More on Loops/07.cpp
+++ C3/C3 10 AS 04 More on Loops/07.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 int main()
 {
+    int n, rem, bin=0, i=1;
+    cout<<"Decimal Number: ";
+    cin>>n;
+    while(n)
+    {
+        rem=n%2;
+        bin=bin+(rem*i);
+        i=i*10;
+        n=n/2;
+    }
+    cout<<"Binary  Number: "<<bin;
     return 0;
 }

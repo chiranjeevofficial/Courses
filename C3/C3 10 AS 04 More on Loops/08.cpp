e63f951062144ad8bcee34641aae9599363ac54c
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-20 11:13:17 +0530

    Update 08.cpp

--- C3/C3 10 AS 04 More on Loops/08.cpp
+++ C3/C3 10 AS 04 More on Loops/08.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 int main()
 {
+    int n, rem, oct=0, i=1;
+    cout<<"Decimal Number: ";
+    cin>>n;
+    while(n)
+    {
+        rem=n%8;
+        oct=oct+(rem*i);
+        i=i*10;
+        n=n/8;
+    }
+    cout<<"Octal Number: "<<oct;
     return 0;
 }

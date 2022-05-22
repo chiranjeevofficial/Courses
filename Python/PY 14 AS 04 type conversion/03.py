52c3e3bac37a87530bde573d7cf2bf030a4b0544
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-05-23 02:12:09 +0530

    PY 14 AS 04 type conversion

---
+++ Python/PY 14 AS 04 type conversion/03.py
@@ -0,0 +1,5 @@
+"""3. Write a python script to print Unicode of the character ‘m’"""
+var1 = 'm';
+var2 = 109;
+print(ord(var1));
+print(chr(var2));
---
+++ Python/PY 14 AS 04 type conversion/05.py
@@ -0,0 +1,3 @@
+"""5. Write a python script to print any number and its octal equivalent."""
+var = 10;
+print(oct(var));

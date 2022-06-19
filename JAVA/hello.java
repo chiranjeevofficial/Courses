cfbeccd98ea07edf427f7708dce10a786aaee400
iamchiranjeev <87925969+iamchiranjeev@users.noreply.github.com>
2022-06-19 18:31:08 +0530

    commit to main

---
+++ JAVA/hello.java
@@ -0,0 +1,5 @@
+class Hello{
+    public static void main(String arga[]) {
+        print("Namaste Java");
+    }
+}
---
+++ JavaScipt/CMNTY JS/script.js
@@ -0,0 +1,20 @@
+console.log("Namaste JavaScript")
+
+// Variable Declaration
+
+// we are does not change value of "const" variable
+const userName = "Chiranjeev Kashyap";
+// var never ever user "var" keyword
+console.log(userName)
+
+let userMobile = 7983499388
+let isLoggesIn = true
+
+let testMeLet
+var testMevar
+
+// var testMeVar : Not a recommended way to declare variables
+console.log(testMeLet) // that have undefined value
+console.log(testMevar) // that have undefined value
+
+// Lexical Structure
